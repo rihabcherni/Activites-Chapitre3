4a3261bd02d6860b7bfe07e95943767bb1bdf6a7
Rihab cherni <rihabcherni000@gmail.com>
2021-10-30 21:16:18 +0200

    Activités Chapitre3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import CaseCocher from './components/CaseCocher.js';
 function App() {
+  const date=  new Date();
+  const hours= date.getHours();
+ let msg;  
+  const h1Style={
+    fontSize:100
+  }
+  if (hours < 12){
+    msg="bonjour"
+    h1Style.color="#04567F"
+  }
+  else if (hours <18) {
+     msg="bon apres-midi"
+     h1Style.color="#0914A3"
+  }
+  else{
+   msg="bonne nuit"
+   h1Style.color="#D940000"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1 style={h1Style}>{msg}</h1>
+     <div className="css-liste">
+          <CaseCocher/>
+          <CaseCocher/>
+          <CaseCocher/>
+          <CaseCocher/>
+     </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/CaseCocher.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function CaseCocher(){
+    return(
+        <div className="css-objet">
+           <input type="checkbox"/>cocher la bonne réponse
+        </div>
+    )
+}
+export default CaseCocher;
